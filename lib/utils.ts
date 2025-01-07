0a6bf8a2fdafa7b650e1eafeb3f062a27b728cfa
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2025-01-07 11:46:15 +0500

    Search Params

--- lib/utils.ts
+++ lib/utils.ts
@@ -58,12 +58,43 @@ interface UrlQueryParams {
 	params: string
 	key: string
 	value: string | null
+	toCourses?: boolean
 }
 
-export const formUrlQuery = ({ key, value, params }: UrlQueryParams) => {
+export const formUrlQuery = ({
+	key,
+	value,
+	params,
+	toCourses = false,
+}: UrlQueryParams) => {
 	const currentUrl = qs.parse(params)
 
 	currentUrl[key] = value
+	return qs.stringifyUrl(
+		{
+			url: toCourses
+				? `${window.location.pathname.split('/')[1]}/courses`
+				: window.location.pathname,
+			query: currentUrl,
+		},
+		{ skipNull: true }
+	)
+}
+
+interface RemoveUrlQueryParams {
+	params: string
+	keysToRemove: string[]
+}
+export const removeKeysFromQuery = ({
+	params,
+	keysToRemove,
+}: RemoveUrlQueryParams) => {
+	const currentUrl = qs.parse(params)
+
+	keysToRemove.forEach(key => {
+		delete currentUrl[key]
+	})
+
 	return qs.stringifyUrl(
 		{
 			url: window.location.pathname,
